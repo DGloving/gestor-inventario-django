209b781d099cd6fa72b449bbf9905161b1690fa8
DGloving <diogopaim2008@hotmail.com>
2026-02-21 10:51:51 -0300

    DB sincronizado, superuser criado e logica de signals testada

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Category, Supplier, Product, Sale
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Supplier)
+
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'total_price', 'sale_date')
+    list_filter = ('sale_date', 'product')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'sku', 'category', 'stock_quantity', 'price_cost', 'price_sell', 'lucro')
+    list_filter = ('category', 'supplier')
+    search_fields = ('name', 'sku')
+
+    def lucro(self, obj):
+        return f"R$ {obj.price_sell - obj.price_cost}"
+    
+    lucro.short_description = 'Lucro Unitário'
--- core/models.py
+++ core/models.py
@@ -1,32 +1,46 @@
 from django.db import models
 
 
-class Supplier(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
-    contact = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=200)
     email = models.EmailField()
+    phone = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
     
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    stock_quantity = models.IntegerField(default=0)
-    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(max_length=200)
+    sku = models.CharField(max_length=50, unique=True) # Código único
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    price_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    price_sell = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
 
 
 class Sale(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     sale_date = models.DateTimeField(auto_now_add=True)
-    total_value = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+
 
     def save(self, *args, **kwargs):
-        self.valor_total = self.quantity * self.product.unit_price
+        self.total_price = self.product.price_sell * self.quantity
         super().save(*args, **kwargs)
-
     
     def __str__(self):
-        return f"Venda de {self.product.name} - {self.sale_date.strftime('%d/%m/%Y')}"
+        return f"Venda: {self.product.name} ({self.quantity})"
